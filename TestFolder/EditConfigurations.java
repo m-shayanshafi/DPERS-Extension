0d9b0366702da493cddf61b39255da937e786c08
Shamsa Abid <shamsaabid123@gmail.com>
2016-01-28 17:09:36 +0500

    deleting class files

--- TestFolder/EditConfigurations.java
+++
@@ -1,8 +0,0 @@
-package AbstractFactoryWithSingleton;
-
-public interface EditConfigurations {
-	public EditMode edit();
-	public FontSelect font();
-	public SpellCheckerUtility spellChecker();
-	public ThesaurusActivate thesaurus();
-}
